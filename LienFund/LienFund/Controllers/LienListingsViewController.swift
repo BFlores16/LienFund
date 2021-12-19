efaaffeb3d58a20b089110415b149eb2838540ed
Brando Flores <brandoflores16@gmail.com>
2021-12-18 22:20:52 -0700

    Add table view to liens listings controller

--- LienFund/LienFund/Controllers/LienListingsViewController.swift
+++ LienFund/LienFund/Controllers/LienListingsViewController.swift
@@ -7,27 +7,37 @@
 
 import UIKit
 
-class LienListingsViewController: UIViewController, UISearchBarDelegate {
+class LienListingsViewController: UIViewController, UISearchBarDelegate, UITableViewDelegate, UITableViewDataSource {
+    
+    final let LIENS_DATA_FILE = "liens_data"
+    var taxLiens = [TaxLien]()
+    var lienListingsCellsViewModels = [LienListingCellViewModel]()
+    let lienListingReusableCellIdentifier = "TaxLienListingTableViewCell"
+    
     @IBOutlet weak var taxLienSearchBar: UISearchBar!
     @IBOutlet weak var taxLienFilterButton: UIButton!
-    
+    @IBOutlet weak var taxLienListingsTableView: UITableView!
     @IBOutlet weak var taxLienListingsTableViewFrame: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // Do any additional setup after loading the view.
+        
+        // This view controller itself will provide the delegate methods and row data for the table view.
+        taxLienListingsTableView.delegate = self
+        taxLienListingsTableView.dataSource = self
+        taxLienListingsTableView.register(UINib(nibName: lienListingReusableCellIdentifier, bundle: nil), forCellReuseIdentifier: lienListingReusableCellIdentifier)
+        
+        parseTaxLienData()
         setupInitialUI()
     }
     
+    func parseTaxLienData() {
+        taxLiens = convertToLiensListings(liensData: readTxtFile(fileName: LIENS_DATA_FILE))
+        lienListingsCellsViewModels = convertTaxLienToLienListingCellViewModels(taxLiens: taxLiens)
+    }
+    
     func setupInitialUI() {
-        // Tab bar
-//        self.tabBarController?.tabBar.layer.shadowOffset = CGSize(width: 0, height: 0)
-//        self.tabBarController?.tabBar.layer.shadowRadius = 8
-//        self.tabBarController?.tabBar.layer.shadowColor = UIColor.black.cgColor
-//        self.tabBarController?.tabBar.layer.shadowOpacity = 0.3
-        //self.tabBarController?.viewDidLayoutSubviews()
-        
-        
         // Nav bar
         navigationController?.navigationBar.prefersLargeTitles = true
         taxLienSearchBar.searchTextField.backgroundColor = UIColor.white
@@ -66,15 +76,79 @@ class LienListingsViewController: UIViewController, UISearchBarDelegate {
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         searchBar.resignFirstResponder()
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return self.lienListingsCellsViewModels.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+//    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+//        let spacing: CGFloat = 1
+//        return spacing
+//    }
+    
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        let spacing: CGFloat = 10
+        return spacing
+    }
+    
+    // create a cell for each table view row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // create a new cell if needed or reuse an old one
+        let cell = self.taxLienListingsTableView.dequeueReusableCell(withIdentifier: lienListingReusableCellIdentifier, for: indexPath) as! TaxLienListingTableViewCell
+            
+        
+        cell.LienNumberLabel.text = "# " + String(self.lienListingsCellsViewModels[indexPath.section].number)
+        cell.LienLocationLabel.text = self.lienListingsCellsViewModels[indexPath.section].county + ", " + self.lienListingsCellsViewModels[indexPath.section].state
+        cell.LienPriceLabel.text = String(self.lienListingsCellsViewModels[indexPath.section].price)
+        cell.InterestRateLabel.text = String(self.lienListingsCellsViewModels[indexPath.section].rate)
 
+        return cell
+    }
+    
+    func readTxtFile(fileName: String) -> [String] {
+        var lines = [String]()
+
+        if let dataURL = Bundle.main.url(forResource: fileName, withExtension: "txt") {
+            if let data = try? String(contentsOf: dataURL) {
+                lines = data.components(separatedBy: "\n")
+            }
+        }
+        lines.remove(at: lines.count - 1)
+        
+        return lines
+    }
+    
+    func convertToLiensListings(liensData: [String]) -> [TaxLien] {
+        var liens = [TaxLien]()
+        
+        for lien in liensData {
+            let lienElements = lien.split(separator: ",")
+
+            let number = Int(lienElements[0]) ?? 0
+            let county = String(lienElements[1])
+            let state = String(lienElements[2])
+            let price = Double(lienElements[3]) ?? 0.0
+            let rate = Double(lienElements[4]) ?? 0.0
+            liens.append(TaxLien(number: number, county: county, state: ShortStates.C[state] ?? "", price: price, rate: rate))
+        }
+        
+        return liens
+    }
+    
+    func convertTaxLienToLienListingCellViewModels(taxLiens: [TaxLien]) -> [LienListingCellViewModel] {
+        var viewModels = [LienListingCellViewModel]()
+        let currencyFormatter = NumberFormatter()
+        currencyFormatter.usesGroupingSeparator = true
+        currencyFormatter.numberStyle = .currency
+        currencyFormatter.locale = Locale.current
+        for lien in taxLiens {
+            viewModels.append(LienListingCellViewModel(number: lien.number, state: lien.state, county: lien.county, price: currencyFormatter.string(from: NSNumber(value: lien.price))!, rate: String(format: "%.1f%%", lien.rate)))
+        }
+        return viewModels
+    }
 }
