68b71507a02e3b4c9575aaef9f4ec1e0350310a2
Brando Flores <brandoflores16@gmail.com>
2022-02-12 00:58:48 -0700

    Fix UI

---
+++ LienFund/LienFund/Models/Cells/DropDownUIView.swift
@@ -0,0 +1,20 @@
+//
+//  DropDownUIView.swift
+//  LienFund
+//
+//  Created by Brando Flores on 1/30/22.
+//
+
+import UIKit
+
+class DropDownUIView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
